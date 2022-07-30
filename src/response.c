4f8f9b933cf0058f059ed2e10fc1df167ee6d2e2
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-07-29 18:57:12 -0700

    Restructure response.c

--- src/response.c
+++ src/response.c
@@ -19,6 +19,84 @@ static const char* JSON_ERROR_CONTENT_FMT = "{\"message\":\"%s\",\"code\":%d}";
 
 static dictionary* status_code_to_string = NULL;
 
+response_t* response_create(http_status status) {
+    response_t* response = malloc(sizeof(response_t));
+    response->status = status;
+    response->headers = string_to_string_dictionary_create();
+
+    return response;
+}
+
+response_t* response_from_file(http_status status, FILE* file) {
+    response_t* response = response_create(status);
+    response->body_content.file = file;
+    response->rt = RT_FILE;
+    
+    return response;
+}
+
+response_t* response_from_string(http_status status, const char* body) {
+    response_t* response = response_create(status);
+    response->body_content.body = body;
+    response->rt = RT_STRING;
+
+    return response;
+}
+
+response_t* response_empty(http_status status) {
+    response_t* response = response_create(status);
+    response->body_content.body = NULL;
+    response->rt = RT_EMPTY;
+
+    return response;
+}
+
+response_t* response_format_error(http_status status, const char* msg) {
+    response_t* response = response_create(status);
+    response->rt = RT_STRING;
+
+    char* buf = NULL;
+    asprintf(&buf, JSON_ERROR_CONTENT_FMT, msg, status);
+    response->body_content.body = buf;
+
+    return response;
+}
+
+response_t* response_bad_request(void) {
+    static const char* msg = "The server was unable to process the request";
+    return response_format_error(STATUS_METHOD_NOT_ALLOWED, msg);
+}
+
+response_t* response_resource_not_found(void) {
+    static const char* msg = "The requested resource was not found";
+    return response_format_error(STATUS_NOT_FOUND, msg);
+}
+
+response_t* response_method_not_allowed(void) {
+    static const char* msg = "The request method is inappropriate for the requested resource";
+    return response_format_error(STATUS_METHOD_NOT_ALLOWED, msg);
+}
+
+void response_destroy(response_t* response) {
+    dictionary_destroy(response->headers);
+    free(response);
+}
+
+void response_set_content_type(response_t* response, const char* content_type) {
+    dictionary_set(response->headers, "Content-Type", (void*) content_type);
+}
+
+void response_set_content_length(response_t* response, size_t length) {
+    char* buf = NULL;
+    asprintf(&buf, "%zu", length);
+    dictionary_set(response->headers, "Content-Length", buf);
+    free(buf);
+}
+
+void response_set_header(response_t* response, const char* key, const char* value) {
+    dictionary_set(response->headers, (void*) key, (void*) value);
+}
+
 static const char* HTTP_STATUS_STRINGS[NUM_HTTP_STATUS_CODES] = {
     "OK",
     "Created",
@@ -108,69 +186,6 @@ void load_status_code_dictionary(void) {
     atexit(cleanup_status_code_to_string_dict);
 }
 
-response_t* response_create(http_status status) {
-    response_t* response = malloc(sizeof(response_t));
-    response->status = status;
-    response->headers = string_to_string_dictionary_create();
-
-    return response;
-}
-
-response_t* response_from_file(http_status status, FILE* file) {
-    response_t* response = response_create(status);
-    response->body_content.file = file;
-    response->rt = RT_FILE;
-    
-    return response;
-}
-
-response_t* response_from_string(http_status status, const char* body) {
-    response_t* response = response_create(status);
-    response->body_content.body = body;
-    response->rt = RT_STRING;
-
-    return response;
-}
-
-response_t* response_empty(http_status status) {
-    response_t* response = response_create(status);
-    response->body_content.body = NULL;
-    response->rt = RT_EMPTY;
-
-    return response;
-}
-
-response_t* response_format_error(http_status status, const char* msg) {
-    response_t* response = response_create(status);
-    response->rt = RT_STRING;
-
-    char* buf = NULL;
-    asprintf(&buf, JSON_ERROR_CONTENT_FMT, msg, status);
-    response->body_content.body = buf;
-
-    return response;
-}
-
-response_t* response_bad_request(void) {
-    static const char* msg = "The server was unable to process the request";
-    return response_format_error(msg, STATUS_METHOD_NOT_ALLOWED);
-}
-
-response_t* response_resource_not_found(void) {
-    static const char* msg = "The requested resource was not found";
-    return response_format_error(msg, STATUS_NOT_FOUND);
-}
-
-response_t* response_method_not_allowed(void) {
-    static const char* msg = "The request method is inappropriate for the requested resource";
-    return response_format_error(msg, STATUS_METHOD_NOT_ALLOWED);
-}
-
-void response_destroy(response_t* response) {
-    dictionary_destroy(response->headers);
-    free(response);
-}
-
 const char* http_status_to_string(http_status status) {
     if ( !status_code_to_string )
         load_status_code_dictionary();
@@ -237,19 +252,4 @@ int response_format_header(response_t* response, char** buffer) {
     free(joined_headers);
 
     return ret;
-}
-
-void response_set_content_type(response_t* response, const char* content_type) {
-    dictionary_set(response->headers, "Content-Type", (void*) content_type);
-}
-
-void response_set_content_length(response_t* response, size_t length) {
-    char* buf = NULL;
-    asprintf(&buf, "%zu", length);
-    dictionary_set(response->headers, "Content-Length", buf);
-    free(buf);
-}
-
-void response_set_header(response_t* response, const char* key, const char* value) {
-    dictionary_set(response->headers, (void*) key, (void*) value);
 }
